72e7c82378cae2732f0633ca2e5509d5a53cd886
Sanaya James <sanaya.j.james@gmail.com>
2026-05-11 16:36:05 -0700

    implemented check session functionality

--- server/app.py
+++ server/app.py
@@ -77,14 +77,22 @@ class Logout(Resource):
     def delete(self):
         cleared = session['user_id'] = None
         return cleared, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
 
+        if user:
+            return UserSchema().dump(user), 200
+        else:
+            return {}, 401
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
